226391b8f0715a5a2b7c1a513c5fbbe971844e31
Yabets Maregn <yabetsanon@gmail.com>
2025-08-04 17:34:15 +0900

    Add Services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <main>
+      <h1>Our Services</h1>
+      <p>Details about the services offered by Kidus Production.</p>
+    </main>
+  );
+}
