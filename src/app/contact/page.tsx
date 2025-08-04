f360fc07d025919a159e133dcfa538e9dc4632d2
Yabets Maregn <yabetsanon@gmail.com>
2025-08-04 17:34:23 +0900

    Add Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <main>
+      <h1>Contact Us</h1>
+      <p>Get in touch with Kidus Production.</p>
+    </main>
+  );
+}
