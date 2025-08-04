82dc0e7b1921aba4f6ed383dd57018d329db9291
Yabets Maregn <yabetsanon@gmail.com>
2025-08-04 17:34:20 +0900

    Add Gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Gallery() {
+  return (
+    <main>
+      <h1>Gallery</h1>
+      <p>Photos and media from Kidus Production events and projects.</p>
+    </main>
+  );
+}
