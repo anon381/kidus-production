03a03990210d153ddabb968f7f0ac3ba2cfd788d
Yabets Maregn <yabetsanon@gmail.com>
2025-08-04 17:34:12 +0900

    Add About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>This is the about page for Kidus Production.</p>
+    </main>
+  );
+}
