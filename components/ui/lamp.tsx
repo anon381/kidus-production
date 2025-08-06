8f27abf4305662dba27594bb6c8ac0209aac6ff7
Yabets Maregn <yabetsanon@gmail.com>
2025-08-07 07:40:12 +0900

    updated

--- components/ui/lamp.tsx
+++ components/ui/lamp.tsx
@@ -16,7 +16,6 @@ export default function LampDemo() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-       Why Hire Me
       </motion.h1>
       
     </LampContainer>
